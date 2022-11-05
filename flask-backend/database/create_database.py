0ee536a8c3cbb5d44f642e4d05f7d50a6ed970a6
Beatriz Paiva <61837675+biafpaiva@users.noreply.github.com>
2022-11-04 21:04:03 -0300

    user admin

--- flask-backend/database/create_database.py
+++ flask-backend/database/create_database.py
@@ -30,22 +30,26 @@ for i in range(len(df_jogos1)):
         'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
         (id, group_, round, day_of_week, date_, time_, stadium, teams, home, visitor, final_score_home, final_score_visitor, is_over, winner))
 
-    df_jogosfinais = pd.read_json("jogos-finais.json")
-    for i in range(len(df_jogosfinais)):
-        id = int(df_jogosfinais['id'][i])
-        round = df_jogosfinais['round'][i]
-        day_of_week = str(df_jogosfinais['weekDay'][i])
-        date_ = str(df_jogosfinais['date'][i])
-        time_ = str(df_jogosfinais['time'][i])
-        stadium = str(df_jogosfinais['stadium'][i])
-        teams = str(df_jogosfinais['teams'][i])
-        home = str(df_jogosfinais['home'][i])
-        visitor = str(df_jogosfinais['visitor'][i])
+df_jogosfinais = pd.read_json("jogos-finais.json")
+for i in range(len(df_jogosfinais)):
+    id = int(df_jogosfinais['id'][i])
+    round = df_jogosfinais['round'][i]
+    day_of_week = str(df_jogosfinais['weekDay'][i])
+    date_ = str(df_jogosfinais['date'][i])
+    time_ = str(df_jogosfinais['time'][i])
+    stadium = str(df_jogosfinais['stadium'][i])
+    teams = str(df_jogosfinais['teams'][i])
+    home = str(df_jogosfinais['home'][i])
+    visitor = str(df_jogosfinais['visitor'][i])
 
-        cur.execute(
-            'INSERT INTO matches_finals (id_, round, day_of_week, date_, time_, stadium, teams, home, visitor) '
-            'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-            (id, round, day_of_week, date_, time_, stadium, teams, home, visitor))
+    cur.execute(
+        'INSERT INTO matches_finals (id_, round, day_of_week, date_, time_, stadium, teams, home, visitor) '
+        'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        (id, round, day_of_week, date_, time_, stadium, teams, home, visitor))
+
+cur.execute('INSERT INTO users (username, email, password) '
+            'VALUES (?, ?, ?)',
+            ("admin", "admin@admin.com", "eusouadm"))
 
 connection.commit()
 connection.close()
